b1f84b0fdbd1e4b92799443fa4cd8856d59a26b7
lpc <lpintocastaneda@gmail.com>
2020-04-04 01:18:01 +0200

    more struct for data

--- annotate.py
+++ annotate.py
@@ -2,25 +2,33 @@ import annotater
 import data
 
 
-def tokenize(text):
+def tokenize_src(text):
+    return text.split()
+
+
+def tokenize_tgt(text):
     return text.split()
 
 
 if __name__ == "__main__":
     id_ = "0"
     lang_target = "en"
-    tokens_src = data.get_tokens(id_, "src")
+    entry = data.get_entry(id_)
+    text_src = entry.text_src
+    text_tgt = entry.get_text(lang_target)
+    tokens_src = text_src.tokens
+    tokens_tgt = text_tgt.tokens
     if not tokens_src:
-        tokens_src = tokenize(data.get_text(id_, "src"))
-    tokens_tgt = data.get_tokens(id_, lang_target)
+        tokens_src = tokenize_src(text_src.str)
     if not tokens_tgt:
-        tokens_tgt = tokenize(data.get_text(id_, lang_target))
-    map_ = data.get_map(id_, lang_target)
-    tokens_src, tokens_tgt, map_ = \
-        annotater.process_manually(tokens_src, tokens_tgt, map_)
-    data.write_tokens(id_, "src", tokens_src)
-    data.write_tokens(id_, lang_target, tokens_tgt)
-    data.write_map(id_, lang_target, map_)
+        tokens_tgt = tokenize_tgt(text_tgt.str)
+    tokens_src, tokens_tgt, map_ = annotater.process_manually(
+        tokens_src, tokens_tgt, entry.get_map(lang_target)
+    )
+    entry.text_src.set_tokens(tokens_src)
+    entry.get_text(lang_target).set_tokens(tokens_tgt)
+    entry.set(lang_target, map_=map_)
 
     if input("Save? [y/N]") in ("y", "Y"):
+        data.write_entry(id_, entry)
         data.commit()
--- data.py
+++ data.py
@@ -1,45 +1,108 @@
 import json
 import os
 
-
-data_path = os.path.join(os.path.dirname(__file__), "data.json")
+data_folder_path = os.path.join(os.path.dirname(__file__), "data")
+data_path = os.path.join(data_folder_path, "data.json")
+TEXTS = {}
 with open(data_path) as f:
-    SENTENCES = json.load(f)
+    TEXTS = json.load(f)
 
 
 def get(id_):
-    return SENTENCES[id_] if id_ in SENTENCES else None
+    return TEXTS[id_] if id_ in TEXTS else None
 
 
 def get_info(id_):
-    return SENTENCES[id_]["info"] if id_ in SENTENCES else None
+    return TEXTS[id_]["info"] if id_ in TEXTS else None
 
 
 def get_all():
-    for i, d in SENTENCES.items():
+    for i, d in TEXTS.items():
         return i, d
 
 
 def write_map(id_, lang, map_):
-    SENTENCES[id_][lang]["map"] = map_
+    TEXTS[id_][lang]["map"] = map_
 
 
 def write_tokens(id_, lang, tokens):
-    SENTENCES[id_][lang]["tokens"] = tokens
+    TEXTS[id_][lang]["tokens"] = tokens
 
 
 def get_map(id_, lang):
-    return SENTENCES[id_][lang]["map"]
+    return TEXTS[id_][lang]["map"]
 
 
 def get_tokens(id_, lang):
-    return SENTENCES[id_][lang]["tokens"]
+    return TEXTS[id_][lang]["tokens"]
 
 
 def get_text(id_, lang):
-    return SENTENCES[id_][lang]["text"]
+    return TEXTS[id_][lang]["text"]
+
+
+def get_entry(id_):
+    return Entry.from_dict(TEXTS[id_])
+
+
+def write_entry(id_, entry):
+    TEXTS[id_] = entry.to_dict()
 
 
 def commit():
     with open(data_path, "w") as f:
-        json.dump(SENTENCES, f)
+        json.dump(TEXTS, f)
+
+
+class Text:
+    def __init__(self, lang, str_, tokens=None):
+        self.lang = lang
+        self.str = str_
+        self.tokens = tokens if tokens else []
+
+    def set_tokens(self, tokens):
+        self.tokens = tokens
+
+    def to_dict(self):
+        return {"text": self.str, "tokens": self.tokens}
+
+
+class Entry:
+    def __init__(self, text_src):
+        self.lang_src = text_src.lang
+        self.text_src = text_src
+        self.texts = {text_src.lang: (text_src, None)}
+
+    def add(self, text, map_):
+        self.texts[text.lang] = (text, map_)
+
+    def get(self, lang):
+        return self.texts[lang]
+
+    def get_text(self, lang):
+        return self.get(lang)[0]
+
+    def get_map(self, lang):
+        return self.get(lang)[1]
+
+    def set(self, lang, text=None, map_=None):
+        text_old, map_old = self.texts[lang]
+        self.texts[lang] = (text if text is not None else text_old,
+                            map_ if map_ is not None else map_old)
+
+    @classmethod
+    def from_dict(cls, d):
+        lang_src = d["info"]["src"]
+        text_src = Text(lang_src, d["src"]["text"], d["src"]["tokens"])
+        entry = cls(text_src)
+        for lang, d_ in d.items():
+            if lang not in ("info", "src"):
+                entry.add(Text(lang, d_["text"], d_["tokens"]), d_["map"])
+        return entry
+
+    def to_dict(self):
+        base = {"info": {"src": self.lang_src},
+                "src": self.text_src.to_dict()}
+        base.update({lang: {**text.to_dict(), "map": map_}
+                     for lang, (text, map_) in self.texts.items() if map_ is not None})
+        return base
